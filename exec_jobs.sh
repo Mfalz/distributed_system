1d5d7768909564f6b643ae3386ff46821aac5ca1
Michele <michelefalzone@gmail.com>
2017-05-01 17:59:43 +0200

    Update exec_jobs.sh

--- exec_jobs.sh
+++ exec_jobs.sh
@@ -5,9 +5,15 @@
 # remove old output files
 $HADOOP_HOME/bin/hdfs dfs -rm -r /output
 
-./bin/get_movies.sh
-./bin/movie_mean.sh 0000068
-./bin/movie_per_year.sh 2000 2002
+# get movie titles
+#./bin/get_movies.sh
+# get mean for the film with id 0000068
+#./bin/movie_mean.sh 0000068
+# get films per year
+#./bin/movie_per_year.sh 2000 2002
+
+# mapreduce job, for each film get the mean rating
+./bin/movies_mean.sh
 
 # remove old local output directory
 rm ./output -r
